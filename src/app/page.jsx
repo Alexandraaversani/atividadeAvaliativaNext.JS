705c64aee17b55383a302af2588f7bc7d9f118f1
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-14 14:29:06 -0300

    feat: criação de hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
+import HeroSection from "./components/heroSection";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+      <div className={styles.mainContent}>
+        <div className={styles.contentArea}>
+          <HeroSection />
+        </div>
+      </div>
       <Footer />
     </div>
   );
